c7d785925245b3dd403c288276a437001941d4d8
Luis Hernández <luis.hernandez@induxsoft.net>
2024-03-27 11:00:27 -0600

    update
    agregar programa de importar version

--- src/catcars/_static/js/entity.js
+++ src/catcars/_static/js/entity.js
@@ -9,9 +9,10 @@ var entity=
     {
         entity.marca=document.getElementById("marca");
         entity.modelo=document.getElementById("ref_car_modelo");
-        if(entity.marca)entity.marca.addEventListener("change",()=>
+
+        if (entity.marca) entity.marca.addEventListener("change", () =>
         {
-            if(entity.marca.value.trim()=="")return;
+            if (entity.marca.value.trim() == "") return;
 
             InduxsoftCrudlModel.InvokeService(entity.url_modelo+"?marca="+entity.marca.value,null,
                 (data)=>
@@ -23,21 +24,27 @@ var entity=
                     alert(error.message?? error);
                 },"GET",false);
         });
+
         this.trigger(entity.marca,"change");
     },
+
     trigger:function(element,event)
 	{
-		var e=new Event(event);
-       if(element)element.dispatchEvent(e);
+	    var e = new Event(event);
+        if (element) element.dispatchEvent(e);
 	},
+
     PrintOptions(data,element)
     {
-        if(!data || !element)return;
-        var html="";
+        if (!data || !element) return;
+
+        var html = "";
+
         for (let i = 0; i < data.length; i++) {
             var itm = data[i];
             html+=`<option value="${itm.sys_pk}">${itm.codigo+" "+itm.descripcion}</option>`;
         }
-        if(element)element.innerHTML=html;
+
+        if (element)element.innerHTML = html;
     }
 }
